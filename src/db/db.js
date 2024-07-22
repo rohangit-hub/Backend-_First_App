46fabd754ff8c3ba3d74cb378398a3810451d58f
ROHAN TIWARI <rohanlinux2020@gmail.com>
2024-07-22 23:24:33 +0530

    db connection

--- src/db/db.js
+++ src/db/db.js
@@ -3,18 +3,20 @@ import dotenv from "dotenv"
 dotenv.config({
     path : '../../env'
 });
-import { DB_NAME } from "../constants";
+// import { DB_NAME } from "../constants";
 
 // require('dotenv').config({path:"../../.env"})
 
 
-const connectionDB = ()=> {
+const connectionDB = async ()=> {
 
     try {
-        const connectionSting = mongoose.connect(`${process.env.DB_CONNECTIO}${DB_NAME}`)  // connection string
+        const connectionSting = await mongoose.connect(process.env.DB_CONNECTIO)  // connection string
         console.log(`Database connected with HOST..! ${connectionSting.connection.host}`)  // to find host 
 
     } catch (error) {
         console.log(`DB connection error..! ${error}`)
     }
 }
+
+export default connectionDB;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import connectionDB from "../src/db"
+
+
+connectionDB()
